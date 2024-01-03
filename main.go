ccc5e7a07b1be9f3751fd52adf613884320394e5
Kaushal-26 <kaushalsolanki260102@gmail.com>
2024-01-03 18:52:26 +0530

    Updates on the making directories and file naming

--- main.go
+++ main.go
@@ -24,9 +24,6 @@ type Data struct {
 	} `json:"tests"`
 }
 
-// Environmental varibles which you can set
-var PORT, FILE_DIRECTORY_PATH, LANGUAGE, STDIN, STDOUT, STDERR string
-
 // Error Handling for the code
 func checkError(e error) {
 	if e != nil {
@@ -36,6 +33,7 @@ func checkError(e error) {
 
 // Make Files
 func makeNewFile(Path string) {
+	fmt.Println("Making a New File: ", Path)
 	f, err := os.Create(Path)
 	checkError(err)
 	defer f.Close()
@@ -45,6 +43,7 @@ func makeNewFile(Path string) {
 func checkDirExistsOrMakeNewDir(Path string) {
 	if _, err := os.Stat(Path); err != nil {
 		if os.IsNotExist(err) {
+			fmt.Println("Making a new Directory: ", Path)
 			err := os.Mkdir(Path, 0755)
 			checkError(err)
 		} else {
@@ -54,7 +53,7 @@ func checkDirExistsOrMakeNewDir(Path string) {
 	os.Chdir(Path)
 }
 
-func makeFilesInSystem(data Data, templateFile []byte) {
+func makeFilesInSystem(data Data, templateFile []byte, LANGUAGE, STDIN, STDOUT string) {
 	// Spliting data as no spaces reqd
 	Site := strings.Split(data.Group, " ")
 
@@ -62,63 +61,64 @@ func makeFilesInSystem(data Data, templateFile []byte) {
 	Path := Site[0] + "/"
 	checkDirExistsOrMakeNewDir(Path)
 
-	// Directory for with the contest name
-	Path = strings.Join(Site[2:], " ")
-	checkDirExistsOrMakeNewDir(Path)
-
-	// Directory for the Problem name
-	checkDirExistsOrMakeNewDir(data.Name)
-
-	// Now Splitting data for name of the problem
-	Site = strings.Split(data.Name, " ")
+	// Directory for with the contest URL
+	Site = strings.Split(data.URL, "/")
+	checkDirExistsOrMakeNewDir(Site[len(Site)-3])
 
 	// Code File with the Problem number like A, B, C1, C2, D... or 1, 2, 3 as per contest
-	codeFile := Site[0][:len(Site[0])-1] + LANGUAGE
+	codeFile := Site[len(Site)-1] + LANGUAGE
 	makeNewFile(codeFile)
 
 	// Writing our template on to the code file
-	err := os.WriteFile(codeFile, templateFile, 0644)
-	checkError(err)
+	if len(templateFile) > 0 {
+		err := os.WriteFile(codeFile, templateFile, 0644)
+		checkError(err)
+	}
 
 	for index, test := range data.Tests {
 		// Index for the testcases as 1, 2, 3...
 		indexToString := strconv.Itoa(index + 1)
 
-		// Making input file
-		inputFile := indexToString + STDIN
-		makeNewFile(inputFile)
-		err := os.WriteFile(inputFile, []byte(test.Input), 0644)
-		checkError(err)
-
-		// Making output file
-		if len(STDOUT) > 0 {
-			outputFile := indexToString + STDOUT
-			makeNewFile(outputFile)
-			err = os.WriteFile(outputFile, []byte(test.Output), 0644)
+		// Making input file if STDIN exists
+		if len(STDIN) > 0 {
+			inputFile := indexToString + STDIN
+			makeNewFile(inputFile)
+			err := os.WriteFile(inputFile, []byte(test.Input), 0644)
 			checkError(err)
 		}
 
-		// Making Error file for the Programming file
-		if len(STDERR) > 0 {
-			errorFile := indexToString + STDERR
-			makeNewFile(errorFile)
+		// Making output file if STDOUT exists
+		if len(STDOUT) > 0 {
+			outputFile := indexToString + STDOUT
+			makeNewFile(outputFile)
+			err := os.WriteFile(outputFile, []byte(test.Output), 0644)
+			checkError(err)
 		}
 	}
 }
 
 func main() {
-	godotenv.Load(".env")
+	// Your .env file where all the varibles are defined
+	current_working_directory, err := os.Getwd()
+	checkError(err)
+	godotenv.Load(current_working_directory + "\\.env")
+
+	// Environmental variables which you will take
+	var PORT, FILE_DIRECTORY_PATH, LANGUAGE, STDIN, STDOUT string
 
 	PORT = os.Getenv("PORT")
 	FILE_DIRECTORY_PATH = os.Getenv("FILE_DIRECTORY_PATH")
 	LANGUAGE = os.Getenv("LANGUAGE")
-	STDIN = os.Getenv("STDIN")
-	STDOUT = os.Getenv("STDOUT")
-	STDERR = os.Getenv("STDERR")
+	STDIN = os.Getenv("STDIN")   // Not Compulsory
+	STDOUT = os.Getenv("STDOUT") // Not Compulsory
 
-	// Taking Argument to make code file with template in whatever language we want
-	var err error
+	if PORT == "" || FILE_DIRECTORY_PATH == "" || LANGUAGE == "" {
+		fmt.Println("Environmental Variables Not Defined!")
+		return
+	}
+
 	var templateFile []byte
+	// Taking Argument to make code file with template in whatever language we want
 	if len(os.Args) > 1 {
 		argsOfProgram := os.Args[1]
 		templateFile, err = os.ReadFile(argsOfProgram)
@@ -143,20 +143,11 @@ func main() {
 		err = c.BindJSON(&data)
 		checkError(err)
 
-		fmt.Println(data)
-
-		// Getting the current directory to resign the current path after making codes file
-		owd, err := os.Getwd()
-		checkError(err)
-
 		// File path to the directory we want to make codes
 		os.Chdir(FILE_DIRECTORY_PATH)
 
 		// Function to make the files on FILE_DIRECTORY_PATH
-		makeFilesInSystem(data, templateFile)
-
-		// Changing our directory back to where we started
-		os.Chdir(owd)
+		makeFilesInSystem(data, templateFile, LANGUAGE, STDIN, STDOUT)
 	})
 
 	// Starting a http server
